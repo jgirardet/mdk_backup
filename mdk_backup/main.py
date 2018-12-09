f230f4864cf726d72288115a097dc66112de2595
jgirardet <ijkl@netc.fr>
2018-12-09 18:58:02 +0100

    texte all

--- mdk_backup/main.py
+++ mdk_backup/main.py
@@ -11,22 +11,32 @@ class FodMerge:
     # def dispatch():
 
 
-def merge(paths): 
+
+class MdkBackup:
+    def __init__(self):
+        pass
+
+    @db_session
+    def generate_pdf_from_texte(self):
+        for patient in Patient.select():
+            texte = patient.content()
+            pdf = pydf.generate_pdf(texte)
+            with open(f'{patient.P_pnom}_{patient.P_pprenom}_{patient.P_pddn}.pdf', 'wb') as f:
+                f.write(pdf)
+        
+
+def merge_existing_pdf(paths): 
     merger = PdfFileMerger()
     for file in paths:
         merger.append(PdfFileReader(open(file, 'rb')))
-    merger.write("output.pdf")
+    merger.write("/home/jimmy/mdk/output.pdf")
 
 
-@db_session
-def gen_py():
-    texte = Patient[1367].content()
-    pdf = pydf.generate_pdf(texte)
-    with open('test_doc.pdf', 'wb') as f:
-        f.write(pdf)
 
 if __name__ == '__main__':
     # p = Path('../fixtures')
     # files = [p / ".".join((str(n), 'pdf')) for n in range(1,4)]
     # merge(files)
-    gen_py()
+    import time
+    
+    MdkBackup().generate_pdf_from_texte()
